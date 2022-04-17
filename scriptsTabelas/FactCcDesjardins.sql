7e0a1d80d5831b6f37232cae83b4a360985bb913
Carlos Viana <carlosaraujoviana@outlook.com>
2022-04-17 18:17:45 -0400

    Datafactory

--- scriptsTabelas/FactCcDesjardins.sql
+++ scriptsTabelas/FactCcDesjardins.sql
@@ -1,15 +1,13 @@
 ﻿CREATE TABLE [dbo].[Fact_Movimentacao]
 (
-	[Pk] INT IDENTITY(1,1) PRIMARY KEY, 
+	[Pk] INT IDENTITY(1,1) PRIMARY KEY,
+	[Cod_Dia] NVARCHAR(50) NULL,
     [DescId] INT NULL, 
     [CatId] INT NULL, 
     [SubCatId] INT NULL, 
-    [TipoId] INT NULL,
-	[ManeiraId] INT NULL,
-	[FormPagId] INT NULL,
-    [CartaoId] INT NULL, 
     [RespId] INT NULL, 
-    [FontId] INT NULL, 
-    [Valor] MONEY NULL, 
-    [Cod_Dia] NVARCHAR(50) NULL
+    [Retrait] MONEY NULL,
+	[Depot] MONEY NULL,
+	[Solde] MONEY NULL
+    
 )
